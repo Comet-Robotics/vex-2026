9b128da6d39ee982f7fb3718af71462fb1b852ba
Jesse Huffine <jessehuffine3735@gmail.com>
2025-10-09 21:57:09 +0000

    setup ROS

---
+++ comet_ros/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'comet_ros'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Jesse Huffine',
+    maintainer_email='jessehuffine3735@gmail.com',
+    description='ROS package to be used on our Jetson Orin Nano',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
