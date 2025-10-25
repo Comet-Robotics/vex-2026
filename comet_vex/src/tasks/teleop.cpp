589cbed633c698a96d50c2610fca2cde2353917d
Keval Shah <kevals2005@gmail.com>
2025-10-24 22:58:13 -0500

    vex brain code for json test

--- comet_vex/src/tasks/teleop.cpp
+++ comet_vex/src/tasks/teleop.cpp
@@ -1,21 +1,48 @@
 #include "tasks/teleop.h"
-#include "liblvgl/llemu.hpp"
-#include "messages/message.pb.h"
+#include "nlohmann/json.hpp"
+#include "pros/misc.h"
+#include "pros/misc.hpp"
+#include "pros/motors.hpp"
+#include "pros/rotation.hpp"
+#include "pros/rtos.hpp"
+
+using json = nlohmann::json;
 
 void opcontrol_initialize() {}
 
-void opcontrol() {
-    // Example usage of the generated protobuf code
-    Person person;
-    person.set_name("John Doe");
-    person.set_id(123);
-    person.set_email("john.doe@example.com");
+void opcontrol() {  
+    json j;
+    pros::Controller master(pros::E_CONTROLLER_MASTER);
+    pros::Rotation odom(13);
+    pros::Motor motor(16);
+
+    odom.reset_position();
 
     while (true) {
-        pros::lcd::clear();
-        pros::lcd::print(1, "Name: %s", person.name().c_str());
-        pros::lcd::print(2, "ID: %d", person.id());
-        pros::lcd::print(3, "Email: %s", person.email().c_str());
-        pros::delay(20);
+        int odom_pos = odom.get_position();
+        int motor_pos = motor.get_position();
+
+        j = {
+            {"odom", odom_pos},
+            {"motor", motor_pos}
+        };
+
+        // send data
+        printf("%s\n", j.dump().c_str());
+
+        // receive data
+        std::string input;
+        std::cin >> input;
+
+
+        if (!input.empty()) {
+            json received = json::parse(input);
+            if (received.contains("motor")) {
+                int voltage = received["motor"];
+                motor.move_voltage(voltage);
+            }
+        }
+
+        pros::delay(10);
     }
 }
