5e8b735ca6c474068b7d081fbabc7269854f238c
Jetson1 <vexu@cometrobotics.org>
2026-01-28 19:49:28 -0600

    removed unnecessary import

--- comet_ros/src/vex_serial_interface/vex_serial_interface/serial_interface.py
+++ comet_ros/src/vex_serial_interface/vex_serial_interface/serial_interface.py
@@ -3,7 +3,6 @@
 import rclpy
 import serial # type: ignore
 from rclpy.node import Node
-from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Pose2D
 from geometry_msgs.msg import Twist
 
