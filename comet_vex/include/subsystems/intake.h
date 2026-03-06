1e31966489a8bded6b00136a3edc999eaab65626
Keval Shah <kevals2005@gmail.com>
2026-03-06 00:01:09 -0600

    removed dejam

--- comet_vex/include/subsystems/intake.h
+++ comet_vex/include/subsystems/intake.h
@@ -103,26 +103,28 @@ public:
                 this->stop();
                 break;
             case IntakeMode::FORWARD:
-                if (isJammed())
-                {
-                    runDeJam();
-                }
-                else
-                {
-                    this->forward();
-                }
+                // if (isJammed())
+                // {
+                //     runDeJam();
+                // }
+                // else
+                // {
+                //     this->forward();
+                // }
 
-                // this->forward();
+                this->forward();
                 break;
             case IntakeMode::FORWARD_SLOW:
-                if (isJammed())
-                {
-                    runDeJam();
-                }
-                else
-                {
-                    this->forwardSlow();
-                }
+                // if (isJammed())
+                // {
+                //     runDeJam();
+                // }
+                // else
+                // {
+                //     this->forwardSlow();
+                // }
+
+                this->forwardSlow();
                 break;
             case IntakeMode::REVERSE:
                 this->reverse();
